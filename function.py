a754073fb9c666f0428d3428e7698982e35fcb59
aleksst85 <65343095+aleksst85@users.noreply.github.com>
2023-03-03 15:07:41 +0700

    home1

--- function.py
+++ function.py
@@ -4,6 +4,7 @@ import json
 def load_posts(posts):
     with open(posts, 'r', encoding='utf-8') as file:
         post_list = json.load(file)
+    for post in post_list:
+        post['content_smail'] =post['content'][:post['content'].find('.',)+1]
     return post_list
-
 
