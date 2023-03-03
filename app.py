c4e509f6f5ddd1a55d606a80f28ee4aa69ee520a
aleksst85 <65343095+aleksst85@users.noreply.github.com>
2023-03-03 17:08:43 +0700

    work 4

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template, send_from_directory
 from config import POST_LIST
-from function import load_posts
+from function import load_posts, find_id_post
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index/')
 def start():
     posts = load_posts(POST_LIST)
-    print(posts)
+    
     return render_template('index.html', posts=posts)
+
+
+
+@app.route("/post/<int:id_post>")
+def post(id_post):
+    post = find_id_post(id_post, POST_LIST)
+    if post!=None:
+        return render_template('post.html', post=post)
+    else:
+        return '<h1>Пост не найден</h1>'
+
+
+
+
+
+
+
 
 
 @app.route("/img/<path:path>")
--- function.py
+++ function.py
@@ -1,4 +1,4 @@
-
+from config import POST_LIST
 import json
 
 def load_posts(posts):
@@ -7,4 +7,19 @@ def load_posts(posts):
     for post in post_list:
         post['content_smail'] =post['content'][:post['content'].find('.',)+1]
     return post_list
+
+
+def find_id_post(id_post, file_post):
+    data = load_posts(file_post)
+    for post in data:
+        if post['pk'] == id_post:
+            return post
+    else:
+        return None
+       
+
+
+
+
+
 
